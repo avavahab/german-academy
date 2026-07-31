f433a915002342e65418925a80f00090b85ea61d
Abdul Vahab <abvahab88@gmail.com>
2026-07-31 11:45:47 +0530

    Fix typescript error in admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -83,7 +83,7 @@ export default function AdminPanel() {
     }
   }, []);
 
-  const saveQuestionsToStorage = (updatedQuestions) => {
+  const saveQuestionsToStorage = (updatedQuestions: any) => {
     setQuestions(updatedQuestions);
     localStorage.setItem("adminQuestionBank", JSON.stringify(updatedQuestions));
 
