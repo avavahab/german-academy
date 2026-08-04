2e7254fee6b14ac0db47bbc5627de917e035a261
Abdul Vahab <abvahab88@gmail.com>
2026-08-04 10:04:50 +0530

    fix: update prisma.config.ts with correct env variable and schema path

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "./prisma/schema.prisma",
   datasource: {
-    url: env("DIRECT_URL"),
+    url: env("DATABASE_URL"), // നിങ്ങളുടെ .env ഫയലിൽ ഡാറ്റാബേസ് ലിങ്ക് സേവ് ചെയ്തിരിക്കുന്ന പേര് (DATABASE_URL അല്ലെങ്കിൽ DIRECT_URL) ഇവിടെ നൽകുക
   },
 });
